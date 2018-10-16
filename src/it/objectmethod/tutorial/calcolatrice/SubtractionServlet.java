e33aa21e34f4a8a0ca30a64caff4d8b72f18d7db
Ivan Maltese <ivan.maltese@objectmethod.it>
2018-10-16 16:09:41 +0200

    CTRL-a, CTRL-i

--- src/it/objectmethod/tutorial/calcolatrice/SubtractionServlet.java
+++ src/it/objectmethod/tutorial/calcolatrice/SubtractionServlet.java
@@ -8,28 +8,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SubtractionServlet extends HttpServlet {
-		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		     // reading the user input
-			String error = null;
-			double result = 0d;
-			try {
-				double n1 = Double.parseDouble(request.getParameter("number1"));
-		        double n2 = Double.parseDouble(request.getParameter("number2"));
-	
-				result = n1 - n2;	 
-			}
-			catch(NumberFormatException e)
-			{
-				error = "String to Number";
-				e.printStackTrace();
-			}   
-			catch(Exception e)
-			{
-				error = "Error";
-				e.printStackTrace();
-			} 
-			
-			request.setAttribute("result", error == null? result:error);
-	       request.getRequestDispatcher("GUI.jsp").forward(request, response);
-}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// reading the user input
+		String error = null;
+		double result = 0d;
+		try {
+			double n1 = Double.parseDouble(request.getParameter("number1"));
+			double n2 = Double.parseDouble(request.getParameter("number2"));
+
+			result = n1 - n2;	 
+		}
+		catch(NumberFormatException e)
+		{
+			error = "String to Number";
+			e.printStackTrace();
+		}   
+		catch(Exception e)
+		{
+			error = "Error";
+			e.printStackTrace();
+		} 
+
+		request.setAttribute("result", error == null? result:error);
+		request.getRequestDispatcher("GUI.jsp").forward(request, response);
 	}
+}
